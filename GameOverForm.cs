70071530cfa1c43c313cc558624af0ba67255766
adriansatentes <145671350+adriansatentes@users.noreply.github.com>
2023-10-24 15:53:11 +0800

    Add files via upload

--- GameOverForm.cs
+++ GameOverForm.cs
@@ -16,22 +16,36 @@ namespace Tetris
    
     public partial class GameOverForm : Form
     {
-        SqlConnection kon = new SqlConnection(); 
         SqlConnection conn;
         SqlCommand cmd; 
-        SqlDataReader rd;
+       
         private int score;
+
         public GameOverForm(int score)
         {
+
             InitializeComponent();
             this.score = score;
             label1.Text = "Score: " + score;
             label2.Text = "Level: " + score / 10;
             label3.Text = "Lines " + score;
-     }
+            conn = new SqlConnection("Data Source=JUSTIN-JOSEPH\\SQLEXPRESS;Initial Catalog=LeaderBoard;Integrated Security=True");
+            conn.Open();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            string playerName = textBox1.Text;
+            string insertQuery = "INSERT INTO LeaderBoard (PlayerName, Score, Level, Lines) VALUES (@PlayerName, @Score, @Level, @Lines)";
 
+            using(cmd =  new SqlCommand(insertQuery, conn))
+            {
+                cmd.Parameters.AddWithValue("@PlayerName", playerName);
+                cmd.Parameters.AddWithValue("@Score", score);
+                cmd.Parameters.AddWithValue("@Level", score / 10);
+                cmd.Parameters.AddWithValue("@Lines", score);
+                cmd.ExecuteNonQuery();
+            }
+            conn.Close();
 
             MainMenu mainMenu = new MainMenu();
             mainMenu.Show(this);
--- Leaderboard.cs
+++ Leaderboard.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Tetris
@@ -15,6 +17,35 @@ namespace Tetris
         public LeaderBoardForm()
         {
             InitializeComponent();
+            LoadData();
+
+            // Change the DataGridView appearance
+            dataGridView1.BackgroundColor = Color.Black;
+            dataGridView1.ForeColor = Color.Red;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; // This makes the columns fill the available width
+            dataGridView1.AllowUserToAddRows = false;
+        }
+
+        private void LoadData()
+        {
+            string connectionString = "Data Source=JUSTIN-JOSEPH\\SQLEXPRESS;Initial Catalog=LeaderBoard;Integrated Security=True";
+            string query = "SELECT * FROM LeaderBoard";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        // Bind the DataTable to the DataGridView
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+            }
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
