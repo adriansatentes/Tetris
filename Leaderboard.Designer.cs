da98c6f71e5357407ff35c5b5f350ed8db9553d3
adriansatentes <145671350+adriansatentes@users.noreply.github.com>
2023-10-24 15:52:29 +0800

    Add files via upload

--- Leaderboard.Designer.cs
+++ Leaderboard.Designer.cs
@@ -38,7 +38,7 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(82, 212);
+            this.dataGridView1.Location = new System.Drawing.Point(79, 212);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(618, 443);
             this.dataGridView1.TabIndex = 0;
@@ -50,7 +50,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button1.Location = new System.Drawing.Point(321, 661);
+            this.button1.Location = new System.Drawing.Point(320, 661);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(142, 61);
             this.button1.TabIndex = 1;
